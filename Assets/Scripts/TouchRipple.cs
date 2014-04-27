ae1164f17ba7128d7c52b5030ec02fa998a1d08d
Richard Locsin <goofydelinquent@gmail.com>
2014-04-27 18:25:32 +0800

    Changed system to prefab and ripples should work ok now.

---
+++ Assets/Scripts/TouchRipple.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchRipple : MonoBehaviour {
+
+	bool bWasButtonDown = false;
+	bool bDidEmit = false;
+	float particleSystemY = 0.1f;
+	public GameObject particleSystem;
+
+	private GameObject currentSystem;
+
+	// Update is called once per frame
+	void Update () {
+
+		// If left mouse button is not held down
+		if ( ! Input.GetMouseButton( 0 ) ) {
+
+			currentSystem.particleSystem.Stop();
+			Destroy( currentSystem, 3f );
+			currentSystem = null;
+			return;
+		}
+
+
+		Vector3 worldPoint = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+		worldPoint.y = particleSystemY;
+
+		if ( currentSystem == null ) {
+
+			currentSystem = Instantiate( particleSystem, worldPoint, Quaternion.identity ) as GameObject;
+
+		} else {
+
+			currentSystem.transform.position = Vector3.Lerp( currentSystem.transform.position, worldPoint, Mathf.Clamp01( 10 *  0.8f * Time.deltaTime ) );
+
+		}
+	}
+}
