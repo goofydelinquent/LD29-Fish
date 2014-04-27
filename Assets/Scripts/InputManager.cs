e70d31f7dbf479fb46a5d60a9c425005059c783f
Richard Locsin <goofydelinquent@gmail.com>
2014-04-27 10:36:22 +0800

    Added 2D movement

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public CharacterController playerController;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movement = Vector2.zero;
+
+		if ( Input.GetButtonDown( "left" ) ) {
+			movement.x -= 1f;
+		}
+
+		if ( Input.GetButtonDown( "right" ) ) {
+			movement.x += 1f;
+		}
+
+	}
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+	public Transform groundCheck;
+
+	public float maxSpeed = 10f;
+	public float moveForce = 3f;
+	public float jumpForce = 5f;
+
+	private bool bIsGrounded = false;
+	private bool bShouldJump = false;
+	private bool bIsFacingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bIsGrounded = Physics2D.Linecast( transform.position, groundCheck.position, 1 << LayerMask.NameToLayer( "Ground" ) );
+
+		if ( Input.GetButtonDown( "Jump" ) && bIsGrounded ) {
+			bShouldJump = true;
+		}
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis( "Horizontal" );
+
+		//TODO set animator parameter for speed
+
+		//Add force if moving
+		if ( h * rigidbody2D.velocity.x < maxSpeed ) {
+			
+			rigidbody2D.AddForce( Vector2.right * h * moveForce );
+			
+		}
+
+		/*
+
+		// If below threshold, speed to zero
+		if ( Mathf.Abs( h ) < 0.2f && bIsGrounded ) {
+
+			rigidbody2D.velocity = new Vector2( 0f, rigidbody2D.velocity.y );
+		
+		}
+		*/
+
+		// cap speed on horizontal axis
+		if ( Mathf.Abs( rigidbody2D.velocity.x ) > maxSpeed ) {
+
+			rigidbody2D.velocity = new Vector2( Mathf.Sign( rigidbody2D.velocity.x ) * maxSpeed, rigidbody2D.velocity.y );
+		}
+
+		// Flip sprite as needed
+		if ( ( h > 0 && !bIsFacingRight ) || ( h < 0 && bIsFacingRight ) ) {
+
+			Flip();
+
+		}
+
+		//
+		if ( bShouldJump ) {
+
+			//TODO trigger jump animation
+			rigidbody2D.AddForce( new Vector2( 0f, jumpForce ) );
+			bShouldJump = false;
+
+		}
+	}
+
+	void Flip () {
+	}
+}
